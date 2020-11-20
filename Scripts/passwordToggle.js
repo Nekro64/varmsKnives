a21baa81ec01c4670e6835683badabf6ff84c74e
Nekro64 <kamil.lechkunn@gmail.com>
2020-11-20 19:51:31 +0100

    Color signal if password is longer or shorten than 6 signs

--- Scripts/passwordToggle.js
+++ Scripts/passwordToggle.js
@@ -1,15 +1,15 @@
 let passInput = document.querySelector("#inputPasswordReg")
 let toggleButton = document.querySelector(".togglePass");
+let passInfoText = document.querySelector(".passInfoText");
 
 
 passInput.addEventListener("keyup", ()=> {
     let displayParameter = passInput.value.length > 0 ? toggleButton.style.display = "flex" : toggleButton.style.display = "none";
-
+    let passCheck = passInput.value.length == 0 ? passInfoText.style.color = "black" : passInput.value.length >= 6 ? passInfoText.style.color = "green" : passInfoText.style.color = "red";
 })
 
 toggleButton.addEventListener("click",()=>{
     let inputType = passInput.getAttribute("type") === "password" ? "text" : "password"
     passInput.setAttribute("type", inputType);
     let buttonContent = toggleButton.textContent === "Pokaż" ? toggleButton.textContent = "Ukryj" : toggleButton.textContent = "Pokaż";
-
 })
